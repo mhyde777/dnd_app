75832ab4884ac34790653b8203b095485344d12e
Mason Hyde <mason_hyde@uri.edu>
2024-07-13 20:36:00 -0400

    Entered a working image widget, for a static image path. Also tried adding functionality to the add combatants button which didn't work. Probably not worth doing anyway since the method I was using relied on the pandas dataframe anyway.

--- lib/ui/logic.py
+++ lib/ui/logic.py
@@ -1,3 +1,8 @@
+from typing import List
+from PyQt5.QtGui import QPixmap 
+from PyQt5.QtWidgets import *
+import pandas as pd
+
 class WidgetLogic:
     def update_active_init(self):
         current_name = self.data.at[self.current_turn, 'Name']
@@ -32,11 +37,57 @@ class WidgetLogic:
         pass
 
     def add_combat(self):
-        pass
+        dialog = AddCombatants(self)
+        if dialog.exec_() == QDialog.Accepted:
+            new_data_list = dialog.get_data()
+            for new_data in new_data_list:
+                self.data = self.data.append(new_data, ignore_index=True)
+            self.data = self.data.sort_values(by='Init', ascending=False).reset_index(drop=True)
+            self.update_table()
 
     def rmv_combat(self):
         pass
 
+    def update_table(self):
+        self.table.setRowCount(len(self.data))
+        for i in range(len(self.data)):
+            for j in range(len(self.data.columns)):
+                self.table.setItem(i, j, QTableWidgetItem(str(self.data.iat[i,j])))
+        self.update_active_init()
 
 
+class AddCombatants(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Add Combatants")
 
+        self.layout = QVBoxLayout(self)
+
+        self.table = QTableWidget(self)
+        self.table.setRowCount(5)  # Allow adding up to 5 creatures at a time
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(['Name', 'Init', 'HP', 'AC'])
+
+        self.layout.addWidget(self.table)
+
+        self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.button_box)
+
+    def get_data(self):
+        data = []
+        for row in range(self.table.rowCount()):
+            name = self.table.item(row, 0)
+            init = self.table.item(row, 1)
+            hp = self.table.item(row, 2)
+            ac = self.table.item(row, 3)
+            if name and init and hp and ac:
+                data.append({
+                    'Name': name.text(),
+                    'Init': int(init.text()),
+                    'HP': int(hp.text()),
+                    'AC': int(ac.text())
+                })
+        return data
