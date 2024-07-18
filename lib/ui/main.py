f713afd9123b3ed48f0cce3be049e8b683ecde6c
Mikhail Hyde <hyde.mikhail@gmail.com>
2024-07-17 20:26:41 -0400

    remove main from lib

--- lib/ui/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from ui import InitiativeTracker
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    mainWin = InitiativeTracker()
-    mainWin.show()
-    sys.exit(app.exec_())
