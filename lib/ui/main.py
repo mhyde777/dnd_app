2359571d48117a759d91b347d4ce2622b2602e69
Mason Hyde <mason_hyde@uri.edu>
2024-07-17 20:23:16 -0400

    Here ya go ya filthy animal

---
+++ lib/ui/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import InitiativeTracker
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWin = InitiativeTracker()
+    mainWin.show()
+    sys.exit(app.exec_())
