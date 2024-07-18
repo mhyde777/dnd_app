3d61c326b667b81a2921f4197b1ae0e7e600e314
Mikhail Hyde <hyde.mikhail@gmail.com>
2024-07-17 20:12:03 -0400

    just some house keeping

---
+++ lib/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dnd_app",
+    version="0.0.1",
+    description="D&D App Library",
+    author="Mason Hyde",
+    author_email="mhyde777@gmail.com",
+    packages=find_packages(),
+)
