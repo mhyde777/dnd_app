aa81256dd5403f595a6a335c454f2df41fabad24
Mason Hyde <mason_hyde@uri.edu>
2024-07-20 16:17:58 -0400

    Active Init and Add Combatant Was able to get the update_active_init working with proper highlighting of the row instead of just row select. I also tried to get add_combatant to work, adding the class AddCombatant. Was unsure if that belonged in app.py or ui.py. It doesn't work yet, I'm not sure why.

--- lib/app/app.py
+++ lib/app/app.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QDialogButtonBox, QPushButton, QTableWidget, QTableWidgetItem
-# from app.manager import CreatureManager
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QColor
+from app.manager import CreatureManager, Player, Monster
 
 class Application:
 
@@ -10,16 +11,39 @@ class Application:
         
         # self.update_active_init()
     #
-    # def update_active_init(self):
-    #     self.current_name = self.manager.creatures.keys()
-    #     self.active_init_label.setText(f"Active: {self.current_name}")
-    #     self.table.selectRow(self.current_turn)
+    def update_active_init(self):
+        self.sorted_creatures = list(self.manager.creatures.values())
+        self.current_creature = self.sorted_creatures[self.current_turn]
+        self.current_name = self.current_creature.name
+        self.active_init_label.setText(f"Active: {self.current_name}")
+        for row in range(self.table.rowCount()):
+            color = QColor(255, 255, 255)
+            if row == self.current_turn:
+                color = QColor(160, 255, 150)
+            self.set_row_color(row, color)
+
+    def set_row_color(self, row, color):
+        for column in range(self.table.columnCount()):
+            self.table.item(row, column).setBackground(color)
 
     def load_encounter(self):
         pass
 
     def add_combatant(self):
-        pass
+        dialog = AddCombatant(self)
+        if dialog.exec_() == QDialog.Accepted:
+            data = dialog.get_data()
+            for creature_data in data:
+                creature = Player(
+                    name=creature_data['Name'],
+                    init=creature_data['Init'],
+                    max_hp=creature_data['HP'],
+                    curr_hp=creature_data['HP'],
+                    armor_class=creature_data['AC']
+                )
+                self.manager.add_creature(creature)
+        self.update_table()
+        self.update_active_init()
 
     def remove_combatant(self):
         pass
@@ -32,7 +56,7 @@ class Application:
             self.time_counter += 6
             self.round_counter_label.setText(f"Round: {self.round_counter}")
             self.time_counter_label.setText(f"Time: {self.time_counter} seconds")
-        # self.update_active_init()
+        self.update_active_init()
 
     def prev_turn(self):
         if self.current_turn == 0:
@@ -44,11 +68,11 @@ class Application:
                 self.time_counter_label.setText(f"Time: {self.time_counter} seconds")
         else:
             self.current_turn -= 1
-        # self.update_active_init()
+        self.update_active_init()
 
     def update_table(self):
         headers = ['CreatureType', 'Name', 'Init', 'Max HP', 'Curr HP', 'Armor Class', 'M', 'A', 'BA', 'R', 'OI', 'Notes', 'Status Time']
-
+        self.manager.sort_creatures()
         self.table.setRowCount(len(self.manager.creatures))
         self.table.setColumnCount(len(headers))
         self.table.setHorizontalHeaderLabels(headers)
@@ -57,3 +81,41 @@ class Application:
         for i, name in enumerate(self.manager.creatures.keys()):
             for j, attr in enumerate(self.manager.creatures[name].__dataclass_fields__):
                 self.table.setItem(i, j, QTableWidgetItem(str(getattr(self.manager.creatures[name], attr))))
+
+class AddCombatant(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Add Combatants")
+
+        self.layout = QVBoxLayout()
+
+        self.add_table = QTableWidget(self)
+        self.add_table.setRowCount(5)
+        self.add_table.setColumnCount(4)
+        self.add_table.setHorizontalHeaderLabels(['Name', 'Init', 'HP', 'AC'])
+
+        self.layout.addWidget(self.add_table, 1)
+
+        self.button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.button_box, 2)
+        self.setLayout(self.layout)
+
+
+    def get_data(self):
+        data = []
+        for row in range(self.add_table.rowCount()):
+            name = self.add_table.item(row, 0)
+            init = self.add_table.item(row, 1)
+            hp = self.add_table.item(row, 2)
+            ac = self.add_table.item(row, 3)
+            if name and init and hp and ac:
+                data.appen({
+                    'Name': name.text(),
+                    'Init': int(init.text()),
+                    'HP': int(hp.text()),
+                    'AC': int(ac.text())
+                })
+        return data
