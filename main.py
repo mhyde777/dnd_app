e72498f7435db022c1858e8f92d8e8c3b5229a26
Mason Hyde <mason_hyde@uri.edu>
2024-08-01 20:32:24 -0400

    Was able to get the load_encounter method working. I also fixed a bug I found where the current_creature_name variable wasn't being initiated so trying to add creatures before using the next/prev buttons would result in an error

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from ui.ui import InitiativeTracker
 import qdarktheme
 
 if __name__ == "__main__":
     qdarktheme.enable_hi_dpi()
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('images/d20_icon.png'))
     qdarktheme.setup_theme("auto")
     mainWin = InitiativeTracker()
     mainWin.showMaximized()
